3debffe0b0d7fc5a22a208656b7b0fcea2feff06
HemanthJabalpuri <hemanthjabalpuri@gmail.com>
2021-12-21 02:17:08 +0000

    RMX2185: Update fingerprint to redfin December

--- init/mt6765.cpp
+++ init/mt6765.cpp
@@ -50,7 +50,7 @@ void setRMX(unsigned int variant)
     RMX218X_props prop[4] = {};
 
     std::string build_desc = "full_oppo6765-user 10 QP1A.190711.020 bedd37e98646d3a1 release-keys";
-    std::string build_fingerprint = "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys";
+    std::string build_fingerprint = "google/redfin/redfin:12/SQ1A.211205.008/7888514:user/release-keys";
 
     // RMX2180
     prop[0] = {
